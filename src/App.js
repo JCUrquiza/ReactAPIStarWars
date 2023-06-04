db0b0785e84fc2bd50992853618b155827aaf71a
Juan <gurquiza8@gmail.com>
2023-06-03 23:36:50 -0500

    connecting by firebase for read and send date of movie

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
 
@@ -14,7 +15,8 @@ function App() {
     setError(null);
 
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-81b99-default-rtdb.firebaseio.com/movies.json');
+      // const response = await fetch('https://swapi.dev/api/films/');
       
       if (!response.ok) {
         throw new Error('Something went wrong!');
@@ -22,15 +24,19 @@ function App() {
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
+      
     } catch (error) {
       setError(error.message);
     }
@@ -42,6 +48,20 @@ function App() {
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-http-81b99-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
+
 
   let content = <p>Found not movies.</p>
 
@@ -59,6 +79,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler} >Fetch Movies</button>
       </section>
